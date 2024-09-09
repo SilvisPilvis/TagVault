13802135b543151e1def3f81fd60e918e180347a
Silvestrs LIgnickis <silvestrsl47@gmail.com>
2024-09-09 22:23:23 +0300

    Replaced tests with Testify asserts

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/test"
+	"github.com/stretchr/testify/assert"
 )
 
 var (
@@ -16,54 +17,42 @@ var (
 
 func TestMainWindowCreation(t *testing.T) {
 	// Check if the window was created
-	if window == nil {
-		t.Error("Main window was not created")
-	}
-
+	assert.NotNil(t, window, "Main window was not created")
 }
 
 func TestWindowIcon(t *testing.T) {
 	// Load the icon
-	icon, err := fyne.LoadResourceFromPath("icon.ico")
-	if err != nil {
-		t.Error("Failed to load icon:", err, "\n")
-	}
+	// Below code causes nil pointer dereference maybe
+	icon, err := fyne.LoadResourceFromPath("./icon.png")
+	assert.NotNil(t, icon, "Icon was not loaded")
+	assert.Nil(t, err, "Error while loading icon: ")
+	// assert.Nil(t, err, "Error while loading icon: ", err.Error())
 
 	// Set the icon
 	window.SetIcon(icon)
 	testApp.SetIcon(icon)
 
 	// Check if the window icon is set
-	if window.Icon() == nil {
-		t.Error("Window icon is not set")
-	}
-
-	if testApp.Icon() == nil {
-		t.Error("App icon is not set")
-	}
-
+	assert.NotNil(t, window.Icon(), "Window icon is not set")
+	assert.NotNil(t, testApp.Icon(), "Window icon is not set")
+	// assert.Nil(t, window.Icon(), "Window icon is not set")
+	// assert.Nil(t, testApp.Icon(), "Window icon is not set")
 }
 
 func TestWindowSize(t *testing.T) {
 	// Check the window size
 	expectedSize := fyne.NewSize(1000, 600)
-	if window.Canvas().Size() != expectedSize {
-		t.Errorf("Expected window size %v, got %v instead", expectedSize, window.Canvas().Size())
-	}
+	assert.Equal(t, expectedSize, window.Canvas().Size(), "Incorrect window size")
 }
 
 func TestWindowContent(t *testing.T) {
 	// Check if the window content is set
-	if window.Content() == nil {
-		t.Error("Window content is not set")
-	}
+	assert.NotNil(t, window.Content(), "Window content is not set")
 }
 
 func TestWindowTitle(t *testing.T) {
 	// Check the window title
-	if window.Title() != "File Explorer" {
-		t.Errorf("Expected window title 'File Explorer', got '%s' instead", window.Title())
-	}
+	assert.Equal(t, "File Explorer", window.Title(), "Incorrect window title")
 }
 
 func setupMainWindowTest(a fyne.App, t *testing.T) fyne.Window {
